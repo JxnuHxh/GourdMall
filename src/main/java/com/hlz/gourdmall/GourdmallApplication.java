b0daede5eb3b81631df377e5e9a75e8628318f9f
hxh <617662163@qq.com>
2019-10-29 09:03:28 +0800

    第一次提交

---
+++ src/main/java/com/hlz/gourdmall/GourdmallApplication.java
@@ -0,0 +1,13 @@
+package com.hlz.gourdmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourdmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GourdmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hlz/gourdmall/GourdmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlz.gourdmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourdmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
