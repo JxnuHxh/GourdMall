2eb9ab6d76afb919898dcadb4d79d53ed43e4468
Davion <davion2017@foxmail.com>
2019-11-16 10:36:22 +0800

    发送验证码到教务在线

--- src/test/java/com/hlz/gourdmall/GourdmallApplicationTests.java
+++ src/test/java/com/hlz/gourdmall/GourdmallApplicationTests.java
@@ -1,13 +1,17 @@
 package com.hlz.gourdmall;
 
+import com.hlz.gourdmall.util.SendCheckCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class GourdmallApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        String result = new SendCheckCode().sendCode("201726705020", "刘勇");
     }
 
 }
