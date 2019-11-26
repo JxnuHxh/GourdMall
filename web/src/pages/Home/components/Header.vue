f70e5d4709f02240d7b6b26003a5eb7b7ed6c74f
LuckyBoy <2927874032@qq.com>
2019-11-26 23:59:18 +0800

    准备编码主页页面

---
+++ web/src/pages/Home/components/Header.vue
@@ -0,0 +1,43 @@
+<!--  -->
+<template>
+<div class=''></div>
+</template>
+
+<script>
+// 例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  // 计算属性
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 方法集合
+  methods: {
+
+  },
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created () {
+
+  },
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted () {
+
+  },
+  beforeCreate () {}, // 生命周期 - 创建之前
+  beforeMount () {}, // 生命周期 - 挂载之前
+  beforeUpdate () {}, // 生命周期 - 更新之前
+  updated () {}, // 生命周期 - 更新之后
+  beforeDestroy () {}, // 生命周期 - 销毁之前
+  destroyed () {}, // 生命周期 - 销毁完成
+  activated () {} // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style scoped>
+
+</style>
