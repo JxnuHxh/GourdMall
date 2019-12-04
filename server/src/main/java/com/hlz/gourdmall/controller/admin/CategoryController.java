3d02c22f1125060815864c0afb2036441c7d9efb
JxnuHxh <you@example.com>
2019-12-04 17:33:18 +0800

    分类

--- server/src/main/java/com/hlz/gourdmall/controller/admin/CategoryController.java
+++ server/src/main/java/com/hlz/gourdmall/controller/admin/CategoryController.java
@@ -2,13 +2,9 @@ package com.hlz.gourdmall.controller.admin;
 
 import com.hlz.gourdmall.dto.Result;
 import com.hlz.gourdmall.enums.ResultCode;
-import com.hlz.gourdmall.model.Category;
 import com.hlz.gourdmall.service.admin.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import java.util.Map;
 
 /**
