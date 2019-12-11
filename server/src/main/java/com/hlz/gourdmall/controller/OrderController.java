5a422312641b4fe254265cacf71f88a09e7e3a79
Xiaohua Huang <617662163@qq.com>
2019-12-11 19:38:18 +0800

    订单模块crud测试并完成

--- server/src/main/java/com/hlz/gourdmall/controller/OrderController.java
+++ server/src/main/java/com/hlz/gourdmall/controller/OrderController.java
@@ -2,10 +2,12 @@ package com.hlz.gourdmall.controller;
 
 import com.hlz.gourdmall.dto.Result;
 import com.hlz.gourdmall.enums.ResultCode;
+import com.hlz.gourdmall.model.Order;
 import com.hlz.gourdmall.service.CategoryService;
 import com.hlz.gourdmall.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,11 +33,27 @@ public class OrderController {
         return new Result(ResultCode.CATEGORY_FIND_SUCCESS, orders);
     }
     @ApiOperation("根据ID查询订单")
-    @GetMapping("/{}")
+    @GetMapping("/{id}")
     public Result getOrderById(@PathVariable(name = "id") String id) {
         Map<String, Object> data = orderService.getOrderById(id);
         return new Result(ResultCode.PRODUCT_FIND_SUCCESS, data);
     }
-
-
+    @ApiOperation("更新订单")
+    @PostMapping("/updateOrder")
+    public  Result updateOrder(Order order){
+        Integer data=orderService.updateOrder(order);
+        return new Result(ResultCode.PRODUCT_FIND_SUCCESS, data);
+    }
+    @ApiOperation("增加订单")
+    @PutMapping("/addOrder")
+    public Result addOrder(Order order){
+        Integer data =orderService.addOrder(order);
+        return new Result(ResultCode.PRODUCT_FIND_SUCCESS, data);
+    }
+    @ApiOperation("删除订单")
+    @DeleteMapping("/deleteOrder")
+    public  Result deleteOrder(String id){
+        Integer data=orderService.deleteOrder(id);
+        return new Result(ResultCode.PRODUCT_FIND_SUCCESS, data);
+    }
 }
--- server/src/main/java/com/hlz/gourdmall/service/OrderService.java
+++ server/src/main/java/com/hlz/gourdmall/service/OrderService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PrimitiveIterator;
 
 /**
  * @author: Hxh
@@ -29,19 +30,43 @@ public class OrderService {
     @Resource
     private OrderMapper orderMapper;
 
-    public Map<String, Object> selectAllOrder(int pageSize,int  pageNum){
+    public Map<String, Object> selectAllOrder(int pageSize, int pageNum) {
         PageHelper.startPage(pageNum, pageSize);
-        Page<Order> orders=orderExtMapper.selectAllOrder();
+        Page<Order> orders = orderExtMapper.selectAllOrder();
         Map<String, Object> data = page2Data.page2Data(orders);
         return data;
     }
-    public Map<String,Object> getOrderById(String id){
-        Order order=orderMapper.selectByPrimaryKey(id);
-        if(order!=null){
-            Map<String,Object> data=new HashMap<>();
-            data.put("data",order);
+
+    public Map<String, Object> getOrderById(String id) {
+        Order order = orderMapper.selectByPrimaryKey(id);
+        if (order != null) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("data", order);
             return null;
         }
         return null;
     }
+
+    public int updateOrder(Order order) {
+        int result = orderMapper.updateByPrimaryKey(order);
+        return result;
+    }
+
+    public int addOrder(Order order) {
+
+        int result = orderMapper.insert(order);
+        return result;
+
+    }
+
+    public int deleteOrder(String id) {
+        Order order = orderMapper.selectByPrimaryKey(id);
+        if (order != null) {
+            int result = orderMapper.deleteByPrimaryKey(id);
+            return result;
+        }
+        return 0;
+
+    }
+
 }
